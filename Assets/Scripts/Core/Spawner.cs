d5b52ce6900edfc3a040d2e78a5282126f7a8086
Malvis Light <cat@lightside.media>
2025-08-04 02:01:56 +0400

    Implemented FieldManager new spawn logic❤️

--- Assets/Scripts/Core/Spawner.cs
+++ Assets/Scripts/Core/Spawner.cs
@@ -1,66 +1,12 @@
 using System;
-using System.Collections.Generic;
-using LSCore.Extensions;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 public class Spawner : MonoBehaviour
 {
-    public List<Shape> shapes;
     [NonSerialized] public Shape currentShape;
-    public List<Block> blockPrefabs => FieldAppearance.BlockPrefabs;
-    public List<Block> specialBlockPrefabs => FieldAppearance.SpecialBlockPrefabs;
-
 
     private void Awake()
     {
         FieldManager.Spawners.Add(this);
-    }
-
-    public Shape SpawnRandomShape(ref int? lastUsedIndex)
-    {
-        if (shapes.Count == 0 || blockPrefabs.Count == 0)
-        {
-            return null;
-        }
-
-        var shapePrefab = shapes.Random();
-        var shapeInstance = Instantiate(shapePrefab, transform.position, Quaternion.identity);
-
-        int newIndex;
-        do
-        {
-            newIndex = Random.Range(0, blockPrefabs.Count);
-        }
-        while (blockPrefabs.Count > 1 && lastUsedIndex.HasValue && newIndex == lastUsedIndex.Value);
-
-        var prefab = blockPrefabs[newIndex];
-        shapeInstance.BlockPrefab = prefab;
-        
-        lastUsedIndex = newIndex;
-        return shapeInstance;
-    }
-    
-    public List<Shape> GetAllShapePrefabs()
-    {
-        return shapes;
-    }
-
-    public Shape SpawnSpecificShape(Shape shapePrefab, ref int? lastUsedIndex)
-    {
-        var shapeInstance = Instantiate(shapePrefab, transform.position, Quaternion.identity);
-
-        int newIndex;
-        do
-        {
-            newIndex = Random.Range(0, blockPrefabs.Count);
-        }
-        while (blockPrefabs.Count > 1 && lastUsedIndex.HasValue && newIndex == lastUsedIndex.Value);
-
-        var prefab = blockPrefabs[newIndex];
-        shapeInstance.BlockPrefab = prefab;
-        
-        lastUsedIndex = newIndex;
-        return shapeInstance;
     }
 }
