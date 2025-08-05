94092b3fa47e47a1f712a9cae90d9283f09f1255
CkBupTaJI <fgmtfit@yandex.ru>
2025-08-06 05:17:21 +1000

    ReWork All levels and reWork Themes 🐺

---
+++ Assets/Scripts/Core/FieldAnimator/Candy/FieldAnimator.Candy.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using DG.Tweening;
+using LSCore.Extensions;
+using UnityEngine;
+
+namespace Core
+{
+    public partial class FieldAnimator
+    {
+        [Serializable]
+        public class Candy : Handler
+        {
+            public ParticleSystem fx;
+            public AnimationCurve anim;
+            public float durScale = .5f;
+
+            public override void Handle()
+            {
+                Sequence seq = DOTween.Sequence();
+                
+                
+                var lines = FieldManager.GetBlockLines(true, true);
+                foreach (var data in lines)
+                {
+                    foreach (var (index, block) in data)
+                    {
+                        var tr = block.transform;
+                        var pos = (index.x + index.y) / 2f * 0.05f;
+                        seq.Insert(pos, tr.DOScale(0f, durScale)
+                            .SetEase(anim).OnComplete(() =>
+                            {
+                                var xueta = tr.position;
+                                xueta.z = -5f;
+                                Instantiate(fx, xueta, fx.transform.rotation);
+                                Destroy(block.gameObject);
+                            }));
+                    }
+                }
+            }
+        }
+    }
+}
