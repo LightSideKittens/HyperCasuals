1670f0d087101cb441b73582adecf7b1798130bb
Malvis Light <cat@lightside.media>
2025-08-14 13:51:11 +0400

    Implemented Ads tools❤️

---
+++ Assets/Scripts/Core/CreoFieldManager.cs
@@ -0,0 +1,42 @@
+﻿using DG.Tweening;
+using LSCore.Extensions.Unity;
+
+namespace Core
+{
+    public class CreoFieldManager : FieldManager
+    {
+        private int spawnedBlockPrefabIndex = 0;
+        
+        protected override void CreateAndInitShape()
+        {
+            activeShapes.Clear();
+            var shapes = allTempShapes[0];
+            if(shapes.Count == 0) return;
+            var blockPrefabs = FieldAppearance.BlockPrefabs;
+            
+            for (int i = 0; i < _spawners.Count; i++)
+            {
+                var tempShape = shapes[0];
+                shapes.RemoveAt(0);
+                SpawnShape();
+                
+                void SpawnShape()
+                {
+                    var shape = Instantiate(tempShape);
+                    activeShapes.Add(shape);
+                    shape.BlockPrefab = blockPrefabs[spawnedBlockPrefabIndex++ % blockPrefabs.Count];
+                    shape.transform.position = _spawners[i].transform.position;
+                    shape.transform.SetScale(shapeSpawnerScale);
+                    _spawners[i].currentShape = shape;
+                }
+            }
+            
+            for (int i = 0; i < activeShapes.Count; i++)
+            {
+                var shape = activeShapes[i];
+                shape.transform.SetScale(0);
+                shape.transform.DOScale(shapeSpawnerScale, 0.2f);
+            }
+        }
+    }
+}
